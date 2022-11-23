44adf2e072d66f8e2bc7c438511564069038230a
LeeJongyul <dlwhddbf1207@naver.com>
2022-11-23 21:39:21 +0900

    Initial commit

---
+++ src/main/java/com/example/Yulss/YulssApplication.java
@@ -0,0 +1,13 @@
+package com.example.Yulss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YulssApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YulssApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Yulss/YulssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Yulss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YulssApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
