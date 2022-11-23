37fa8a7bbe8f2440e6c5c2c38dc223924a303319
LeeJongyul <dlwhddbf1207@naver.com>
2022-11-23 21:57:15 +0900

    공부해야할 내용 주석 추가

--- src/main/java/com/example/Yulss/YulssApplication.java
+++ src/main/java/com/example/Yulss/YulssApplication.java
@@ -8,6 +8,27 @@ public class YulssApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(YulssApplication.class, args);
+
+		/*
+		Yulss
+
+블로그
+
+blog_key
+
+
+
+이미지파일
+
+
+방문자
+카테고리 게시글(좋아요 기능)
+댓글
+쪽지
+
+
+
+		 */
 	}
 
 }
