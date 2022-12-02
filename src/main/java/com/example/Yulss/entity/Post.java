ea77bd33ccb2efe17d70e9bda410621160a7ec0e
LeeJongyul <dlwhddbf1207@naver.com>
2022-12-03 01:40:14 +0900

    Entity 추가 및 수정

--- src/main/java/com/example/Yulss/entity/Post.java
+++ src/main/java/com/example/Yulss/entity/Post.java
@@ -12,7 +12,7 @@ public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="post_key", nullable = false)
-    @Comment("USER KEY")
+    @Comment("POST KEY")
     private Long postKey;
 
     //외래키
@@ -20,8 +20,42 @@ public class Post {
     @JoinColumn(name="user_key")
     private User user;
 
+    @Column(name="create_by")
+    private Long createBy;
+
+    @Column(name="modify_time")
+    private LocalDateTime modifyTime;
+
+    @Column(name="modify_by")
+    private Long modifyBy;
+
+    @Column(name="delete_time")
+    private LocalDateTime deleteTime;
+
+    @Column(name="delete_flag")
+    private Long deleteFlag;
+
+    @Column(name="post_title")
+    private String postTitle;
+
+    @Column(name="post_content")
+    private String postContent;
+
+    @Column(name="post_file")
+    private String postFile;
+
+    @Column(name="post_image")
+    private String postImage;
+
+    @Column(name="post_map")
+    private String postMap;
+
+    @Column(name="post_open")
+    private String postOpen;
 
     @Column(name="create_time")
     private LocalDateTime createTime;
+
+
 
 }
